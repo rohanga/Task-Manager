db6e7f5e55347c7d0ee408ca7119431357fd52a4
optimus <rohan@mortgagekart.com>
2025-06-25 14:22:37 +0530

    backend server url update on frontend env

--- task-manager/src/environments/environment.prod.ts
+++ task-manager/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiurl: 'http://127.0.0.1:8000',
+  apiurl: 'https://task-manager-backend-fijx.onrender.com',
   token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30'
 
 };
--- task-manager/src/environments/environment.ts
+++ task-manager/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiurl: 'http://127.0.0.1:8000',
+  apiurl: 'https://task-manager-backend-fijx.onrender.com',
   token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30'
 };
 
