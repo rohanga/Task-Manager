4695d74f6ff1ef11d4290fb1556d749073a4539b
rohanga <30858021+rohanga@users.noreply.github.com>
2025-08-03 12:24:48 +0530

    Host irl for prd

--- task-manager/src/environments/environment.prod.ts
+++ task-manager/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiurl: 'https://task-manager-backend-fijx.onrender.com',
+  apiurl: 'https://my-fastapi-app-1v32.onrender.com',
   token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30'
 
 };
